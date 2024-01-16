4231891cb06e06964624d9cd096130fe9b978838
muslumhanozturk <muslumhanozturk@gmail.com>
2024-01-16 18:38:35 +0000

    added docker-compose file and script for local deployment

---
+++ test-local-deployment.sh
@@ -0,0 +1,6 @@
+#################################
+# bu script dev server için uygulamaı mysql db kullanarak docker ile
+# ayağa kaldırıyor
+#################################
+
+docker-compose -f docker-compose-local.yml up
